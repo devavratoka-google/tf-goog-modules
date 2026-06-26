a31fd291cf27420625bd99e2513d6f4f6ea12b48
Devavrat Oka <devavratoka@google.com>
2026-06-26 14:09:33 -0600

    cert mgr region 2 update

--- modules/certmgr_issuance_config/main.tf
+++ modules/certmgr_issuance_config/main.tf
@@ -44,7 +44,7 @@ resource "google_certificate_manager_certificate_issuance_config" "this_usw1" {
 
   certificate_authority_config {
     certificate_authority_service_config {
-      ca_pool = "projects/sjc-pr-ssvcs-pki-01/locations/us-west1/caPools/<ca-pool-id>"
+      ca_pool = "projects/<pki-proj-id>/locations/us-east4/caPools/<ca-pool-id>"
     }
   }
 
@@ -53,10 +53,12 @@ resource "google_certificate_manager_certificate_issuance_config" "this_usw1" {
   project  = each.value.project_id
 }
 
-resource "google_privateca_ca_pool_iam_member" "certrequester_usw1" {
-  for_each = { for p in data.google_projects.all_org_projects.projects : p.project_id => p }
-  ca_pool  = "projects/sjc-pr-ssvcs-pki-01/locations/us-west1/caPools/<ca-pool-id>"
-  project  = "pki-proj"
-  role     = "roles/privateca.certificateRequester"
-  member   = "service-${each.value.number}@gcp-sa-certificatemanager.iam.gserviceaccount.com"
-}
+// This is commented for now, until we have the ca pool configured for this region
+
+# resource "google_privateca_ca_pool_iam_member" "certrequester_usw1" {
+#   for_each = { for p in data.google_projects.all_org_projects.projects : p.project_id => p }
+#   ca_pool  = "projects/sjc-pr-ssvcs-pki-01/locations/us-west1/caPools/<ca-pool-id>"
+#   project  = "pki-proj"
+#   role     = "roles/privateca.certificateRequester"
+#   member   = "service-${each.value.number}@gcp-sa-certificatemanager.iam.gserviceaccount.com"
+# }
